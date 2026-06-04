58236dc3c19ac933032bab3e441f17be146cfa79
Michelle Lin <68549082+michelleamazinglin@users.noreply.github.com>
2026-06-03 22:21:16 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let left = 0;
+        let right = s.length - 1;
+
+        while (left < right) {
+            while (left < right && !this.isAlphaNumeric(s[left])) {
+                left++;
+            }
+            while (left < right && !this.isAlphaNumeric(s[right])) {
+                right--;
+            }
+            if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    isAlphaNumeric(char) {
+        return (char >= 'a' && char <= 'z') ||
+        (char >= "A" && char <= "Z") ||
+        (char >= "0" && char <= "9");
+    }
+}
