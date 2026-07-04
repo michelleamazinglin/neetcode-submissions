1e8a1bb8f2d0705adcc02332ac3d4c7d97d1cf7b
Michelle Lin <68549082+michelleamazinglin@users.noreply.github.com>
2026-07-04 14:15:31 -0400

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let left = 0;
+        let right = 1;
+        let profit = 0;
+
+        while (right < prices.length){
+            if (prices[left]<prices[right]){
+                profit = Math.max(profit, prices[right]-prices[left]);
+            } else {
+                left = right;
+            }
+            right++;
+        }
+        return profit
+    }
+}
